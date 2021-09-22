53e0dcf3331e1d2a473a5893a9e4c1cad3c1d360
Navi Kaur <navdeep9520@gmail.com>
2021-09-22 15:00:12 -0700

    commit tree af4bd814a52f933ad9e9ae274fb7937a76dbf514

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+function saturdayFun(activity="roller-skate"){
+    return(`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+saturdayFun(`bathe my dog!`);
+
+function mondayWork(activity='go to the office'){
+    return(`This Monday, I will ${activity}.`)
+}
+mondayWork();
+mondayWork(`I will work from home.`);
+
+function wrapAdjective(star = `*`){
+    return function(adjective = "a hard worker"){
+        return `You are ${star}${adjective}${star}!`;
+    };
+    }
+
+
+wrapAdjective('You are')(`a dedicated programer`) 
+
+
+// wrapAdjective("%")("a dedicated programmer");
+
+//     it("when initialized with '*' creates a function that, when called, wraps an adjective in a highlight", function() {
+//       let result = wrapAdjective()
+//       let emphatic = result("a hard worker")
+//       expect(emphatic).to.equal("You are *a hard worker*!")
+//     });
+
+//     it("when initialized with '||' creates a function that, when called, wraps an adjective in a highlight", function() {
+//       let result = wrapAdjective("||")
+//       let emphatic = result("a dedicated programmer")
+//       expect(emphatic).to.equal("You are ||a dedicated programmer||!")
+//     });
+//   })
+// })
